0c73701904460a241689cb7ac42c3f71d5e76139
jiainho <jia.inho.nehe@gmail.com>
2025-04-30 21:35:30 +0900

    Update Buoi16_Baitap.sql

--- Buoi16_Baitap.sql
+++ Buoi16_Baitap.sql
@@ -6,12 +6,31 @@ where (customer_id, order_date) in (
   from Delivery
   group by customer_id
 );
--- EX 02
-
+-- EX 02 ??
+select
+round(count(distinct player_id) / (select count(distinct player_id) from activity), 2) as fraction
+from activity
+where (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) 
+IN (
+    select player_id, MIN(event_date) AS first_login FROM activity
+    Group by player_id
+)
 -- EX 03
-
+select id,
+case
+when id % 2= 0 then lag(student) over(order by id)
+else coalesce(lead(student) over(order by id), student)
+end as student
+from seat
 -- EX 04
+SELECT visited_on, amount, average_amount 
+FROM (
+    SELECT DISTINCT visited_on, 
+    SUM(amount) OVER (ORDER BY visited_on RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW) AS amount,
+    ROUND(SUM(amount) OVER (ORDER BY visited_on RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW)/7,2) AS average_amount
 
+FROM Customer) as whole_totals
+WHERE DATEDIFF(visited_on, (SELECT MIN(visited_on) FROM Customer)) >= 6
 -- EX 05
 
 -- EX 06
